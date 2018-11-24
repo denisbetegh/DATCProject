602429eaff48df0f03af32ff2a2e55f6db2b75d2
denisbetegh <denis.betegh@gmail.com>
2018-11-24 11:49:05 +0200

    OnlineDashboard Created

---
+++ app/Irrigation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use DB;
+class Irrigation extends Model
+{
+    protected $table='irrigation';
+    protected $fillable = [
+        'temp1', 'humi1', 'temp2', 'humi2', 'temp3', 'humi3', 'temp4', 'humi4'
+    ];  
+}
---
+++ database/migrations/2018_11_24_094017_create_irrigations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIrrigationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('irrigations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('temp1');
+            $table->integer('humi1');
+            $table->integer('temp2');
+            $table->integer('humi2');
+            $table->integer('temp3');
+            $table->integer('humi3');
+            $table->integer('temp4');
+            $table->integer('humi4');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('irrigations');
+    }
+}
