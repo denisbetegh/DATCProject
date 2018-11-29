fae3e2592742bc3ca01e6b5aa657ed3976bec2a7
denisbetegh <denis.betegh@gmail.com>
2018-11-29 16:59:34 +0200

    Colored the Stadium

--- resources/views/irrigation/index.blade.php
+++ resources/views/irrigation/index.blade.php
@@ -20,11 +20,11 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-xl-6 mt-4">
-                    <div id="map" class="centered"></div>
+                    <div id="map" class="centered" style="height:500px;"></div>
                     <p class="font-weight-bold text-center"> Temperature</p>
                 </div>
                 <div class="col-xl-6 mt-4 ">
-                    <div id="map2" class="centered"></div>
+                    <div id="map2" class="centered" style="height:500px;"></div>
                     <p class="font-weight-bold text-center"> Humidity</p>
                  </div>
             </div>
@@ -42,11 +42,11 @@
             var map2;
             function initialize() {
               var mapOptions = {
-                zoom: 8,
+                zoom: 15,
                 center: {lat: 45.7494, lng: 21.2272}
               };
               var mapOptions2 = {
-                zoom: 8,
+                zoom: 15,
                 center: {lat: 45.7494, lng: 21.2272}
               };
               map = new google.maps.Map(document.getElementById('map'),
@@ -54,6 +54,33 @@
               map2 = new google.maps.Map(document.getElementById('map2'),
                   mapOptions2);
             //   Google Marker settings
+
+ var flightPlanCoordinates = [
+    {lat:45.74113992353151, lng:21.24396463057269},
+    {lat:45.74112581763608, lng:21.244240898101225},
+    {lat:45.74105445762709, lng:21.244399148433104},
+    {lat:45.740969445796566, lng:21.244498390166655},
+    {lat:45.74006690571486, lng:21.244839030711546},
+    {lat:45.73991714928079, lng:21.24474247118701},
+    {lat:45.73978985599603, lng:21.244560080973997},
+    {lat:45.73976739244505, lng:21.244334775416746},
+    {lat:45.7397823681467, lng:21.244163114039793},
+    {lat:45.73984975875442, lng:21.24399145266284},
+    {lat:45.74076326785684, lng:21.243626672236815},
+    {lat:45.741020951198024, lng:21.24371920844783},
+    {lat:45.74113992353151, lng:21.24396463057269},
+  ];
+  var flightPath = new google.maps.Polygon({
+    path: flightPlanCoordinates,
+    geodesic: true,
+    strokeColor: '#FF0000',
+    strokeOpacity: 1.0,
+    strokeWeight: 2   
+    ,fillColor: '#FF0000',
+    fillOpacity: 0.35
+  });
+
+  flightPath.setMap(map);
 
 
               /* var marker = new google.maps.Marker({
